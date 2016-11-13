a513fc5f2d884f9bcdca027cdab1773f26ca844e
Cristian Militaru <cristipiticul@yahoo.com>
2016-11-13 19:07:19 +0200

    update (should add comments)

--- matrix_transform/src/real_time_transform.cpp
+++ matrix_transform/src/real_time_transform.cpp
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+#include <std_msgs/String.h>
 // PCL specific includes
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
@@ -73,9 +74,67 @@ std::vector<int> inliers_sphere;
 std::vector<int> inliers_plan;
 pcl::PointIndices::Ptr inliers_cylinder(new pcl::PointIndices);
 
+Eigen::VectorXf ground_coefficients;
+
 pcl::PointCloud<PointType>::Ptr cloud_outliers(new PointCloud<PointType>());
 
 pcl::PointIndices::Ptr inliers_pi(new pcl::PointIndices);
+
+
+
+enum ShapeType {
+    BOX, CYLINDER, SPHERE, PERSON, UNKNOWN, OUR_OBJECT, NOT_OUR_OBJECT
+};
+
+struct Shape
+{
+    ShapeType type;
+    hsv color;
+    vector<double> coefficients;
+    PointXYZ center;
+    PointXYZ pointMin;
+    PointXYZ pointMax;
+};
+
+struct ObjectFilter {
+    ShapeType shapeType;
+    int index;
+};
+
+ObjectFilter command;
+bool commandReceived = false;
+bool shouldDetect = true;
+
+double hDist(double h1, double h2) {
+    double minH = (h1 < h2 ? h1 : h2);
+    double maxH = (h1 >= h2 ? h1 : h2);
+
+    double d1 = maxH - minH;
+    double d2 = minH + 360.0 - maxH;
+    return (d1 < d2 ? d1 : d2);
+}
+
+string getColor(hsv color) {
+    if (color.s < 0.1 || color.v < 0.1) {
+        if (color.v > 0.5) {
+            return "white";
+        }
+        else {
+            return "black";
+        }
+    }
+    double distRed = hDist(color.h, 0);
+    double distGreen = hDist(color.h, 100);
+    double distBlue = hDist(color.h, 230);
+
+    if (distRed < distGreen && distRed < distBlue) {
+        return "red";
+    }
+    if (distGreen < distBlue) {
+        return "green";
+    }
+    return "blue";
+}
 
 hsv rgb2hsv(rgb in) {
     hsv out;
@@ -118,7 +177,6 @@ hsv rgb2hsv(rgb in) {
     return out;
 }
 
-const char* figuriArray[] = { "plane", "sphere", "cylinder", "other" };
 const char* colorArray[] = { "red", "green", "blue" };
 
 void passthroughFilter() //passthrough filter
@@ -131,11 +189,12 @@ void passthroughFilter() //passthrough filter
     pass.filter(*cloud_filtered);
 }
 
+// input: cloud_object
 hsv colorDetection() {
     int sumR = 0, sumG = 0, sumB = 0, n = 0;
 
-    for (int i = 0; i < cloud_filtered->points.size(); i++) {
-        PointType &p = cloud_filtered->points[i];
+    for (int i = 0; i < cloud_object->points.size(); i++) {
+        PointType &p = cloud_object->points[i];
         uint32_t rgb = *reinterpret_cast<int*>(&p.rgb);
         uint8_t r = (rgb >> 16) & 0x0000ff;
         uint8_t g = (rgb >> 8) & 0x0000ff;
@@ -154,7 +213,6 @@ hsv colorDetection() {
     color.g = avgG / 255.0;
     color.b = avgB / 255.0;
     hsv color_hsv = rgb2hsv(color);
-    cout << color_hsv.h << " " << color_hsv.s << " " << color_hsv.v << '\n';
 
     return color_hsv;
 }
@@ -164,7 +222,7 @@ void planDetection() {
             new pcl::SampleConsensusModelPlane<PointType>(cloud_object));
 
     pcl::RandomSampleConsensus<PointType> ransac(model_p);
-    ransac.setDistanceThreshold(.01);
+    ransac.setDistanceThreshold(.02);
     ransac.computeModel();
     ransac.getModelCoefficients(coefficientsInliers);
     ransac.getInliers(inliers_plan);
@@ -176,7 +234,7 @@ void sphereDetection() {
             new pcl::SampleConsensusModelSphere<PointType>(cloud_object));
 
     pcl::RandomSampleConsensus<PointType> ransac(model_s);
-    ransac.setDistanceThreshold(.01);
+    ransac.setDistanceThreshold(.02);
     model_s->setRadiusLimits(0.0, 0.5);
     ransac.computeModel();
     ransac.getInliers(inliers_sphere);
@@ -229,9 +287,6 @@ void voxelGrid()
       sor.setInputCloud(cloud_filtered);
       sor.setLeafSize (0.01f, 0.01f, 0.01f);
       sor.filter(*cloud_filtered2);
-
-      std::cerr << "PointCloud after filtering: " << cloud_filtered->width * cloud_filtered->height
-           << " data points (" << pcl::getFieldsList (*cloud_filtered) << ").";
 }
 
 void getOutliers(PointCloud<PointType>::Ptr cloud_in, vector<int> inliers, PointCloud<PointType>::Ptr cloud_out) {
@@ -258,7 +313,7 @@ bool planSegmentation() {
     // Mandatory
     seg.setModelType(pcl::SACMODEL_PLANE);
     seg.setMethodType(pcl::SAC_RANSAC);
-    seg.setDistanceThreshold(0.01);
+    seg.setDistanceThreshold(0.02);
 
     seg.setInputCloud(cloud_segmented);
     seg.segment(*outliersSegm, *coefficientsOutliers);
@@ -318,50 +373,74 @@ boost::shared_ptr<pcl::visualization::PCLVisualizer> simpleVis(
     return (viewer);
 }
 
-string detectShape() {
+string shapeTypeToString(ShapeType type)
+{
+    switch (type)
+    {
+    case CYLINDER:
+        return string("cylinder");
+    case SPHERE:
+        return string("sphere");
+    case BOX:
+        return string("box");
+    case PERSON:
+        return string("person");
+    case UNKNOWN:
+        return string("unknown");
+    case OUR_OBJECT:
+        return string("our_object");
+    case NOT_OUR_OBJECT:
+        return string("not_our_object");
+    default:
+        return string("PROBLEM IN shapeTypeToString");
+    }
+}
 
-    //colorDetection();
-
-    /*if (pcl::console::find_argument(argc, argv, "-f") >= 0) {
-     planDetection();
-     } else if (pcl::console::find_argument(argc, argv, "-sf") >= 0) {
-     sphereDetection();
-     } else if (pcl::console::find_argument(argc, argv, "-cyl") >= 0) {
-     cylinderDetection();
-     }*/
+Shape detectShape() {
+    Shape res;
 
     const double ratio = 60.0 / 100;
     const int minPoints = (int) (cloud_object->points.size() * ratio);
+    const int min_remaining_points_for_object = 200;
+    int bestPoints = 0;
+    bool nocheck = false;
+    string bestPointsString;
     sphereDetection();
-    if (inliers_sphere.size() >= minPoints) {
-        //sfera
-        return string("sfera");
-    } else {
-        cylinderDetection();
-        if (inliers_cylinder->indices.size() >= minPoints) {
-            //cilindru
-            return string("cilindru");
-        } else {
-            planDetection();
-            if (inliers_plan.size() >= minPoints) {
-                //plan
-                return string("plan");
-            } else {
-                //object (box)
-                getOutliers(cloud_object, inliers_plan, cloud_outliers);
-                if (planSegmentation()) {
-                    return string("obiect");
-                } else {
-                    return string("necunoscut");
-                }
-            }
+    if (inliers_sphere.size() > bestPoints) {
+        bestPoints = inliers_sphere.size();
+        res.type = SPHERE;
+    }
+
+    cylinderDetection();
+    if (inliers_cylinder->indices.size() > bestPoints) {
+        bestPoints = inliers_cylinder->indices.size();
+        res.type = CYLINDER;
+    }
+
+    planDetection();
+    if (inliers_plan.size() > bestPoints) {
+        bestPoints = inliers_plan.size();
+        res.type = BOX;
+    }
+
+    //object (box)
+    getOutliers(cloud_object, inliers_plan, cloud_outliers);
+    if (cloud_outliers->points.size() >= min_remaining_points_for_object) {
+        if (planSegmentation()) {
+            res.type = BOX;
+            nocheck = true;
         }
     }
-    cout << '\n';
+    if (!nocheck && bestPoints < minPoints) {
+        // TODO: unknown, not person
+        res.type = PERSON;
+    }
+
+    return res;
 }
 
 // input: cloud_filtered
-// output: cloud_without_ground
+// output: cloud_without_ground, ground_coefficients
 void findGround()
 {
     pcl::SampleConsensusModelPlane<PointType>::Ptr model_p(
@@ -370,13 +449,42 @@ void findGround()
     pcl::RandomSampleConsensus<PointType> ransac(model_p);
     ransac.setDistanceThreshold(.01);
     ransac.computeModel();
-    ransac.getModelCoefficients(coefficientsInliers);
+    ransac.getModelCoefficients(ground_coefficients);
     ransac.getInliers(inliers_plan);
-    getOutliers(cloud_filtered2, inliers_plan, cloud_without_ground);
+    double groundPlaneYNormal = abs(ground_coefficients[1]);
+    if (groundPlaneYNormal > 0.9) { // it is vertical
+        //cout << "Ground detected!\n";
+        getOutliers(cloud_filtered2, inliers_plan, cloud_without_ground);
+    }
+    else {
+        //cout << "Ground not detected!\n";
+        copyPointCloud(*cloud_filtered2, *cloud_without_ground);
+    }
+}
+
+bool cmpByX(const Shape& s1, const Shape& s2) {
+    return s1.center.x < s2.center.x;
 }
 
 boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer("3D viewer"));
+boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer_raw (new pcl::visualization::PCLVisualizer("Raw"));
 // input: cloud_without_ground
+PointXYZ last_location;
+
+double distance_xyz(PointXYZ p1, PointXYZ p2) {
+    return sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y) + (p1.z - p2.z) * (p1.z - p2.z));
+}
+
+double MAX_DISTANCE = 0.3;
+ShapeType checkOurObject(PointXYZ center) {
+    if (distance_xyz(last_location, center) < MAX_DISTANCE) {
+        return OUR_OBJECT;
+    }
+    else {
+        return NOT_OUR_OBJECT;
+    }
+}
+
 void euclidianClustering() {
     pcl::search::KdTree<PointType>::Ptr tree(
             new pcl::search::KdTree<PointType>);
@@ -384,7 +492,7 @@ void euclidianClustering() {
 
     std::vector<pcl::PointIndices> cluster_indices;
     pcl::EuclideanClusterExtraction<PointType> ec;
-    ec.setClusterTolerance(0.02); // 2cm
+    ec.setClusterTolerance(0.03); // 2cm
     ec.setMinClusterSize(1000);
     //ec.setMaxClusterSize(25000);
     ec.setSearchMethod(tree);
@@ -392,13 +500,7 @@ void euclidianClustering() {
     ec.extract(cluster_indices);
 
     int j = 0;
-    viewer->removeAllPointClouds(0);
-    pcl::visualization::PointCloudColorHandlerRGBField<PointType> handler(
-            cloud_without_ground);
-    viewer->addPointCloud<PointType>(cloud_without_ground, handler, "sample cloud");
-    viewer->setPointCloudRenderingProperties(
-            pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "sample cloud");
-    cout << cluster_indices.size() << "clusters\n";
+    vector<Shape> shapes[10];
     for (std::vector<pcl::PointIndices>::const_iterator it =
             cluster_indices.begin(); it != cluster_indices.end(); ++it) {
         PointXYZ pointMin(99999.0f, 99999.0f, 99999.0f);
@@ -441,39 +543,139 @@ void euclidianClustering() {
         cloud_object->height = 1;
         cloud_object->is_dense = true;
 
+        Shape shape;
+        if (shouldDetect) {
+            shape = detectShape();
+        } else {
+            shape.type = checkOurObject(centroid);
+            if (shape.type == OUR_OBJECT) {
+                last_location = centroid;
+            }
+        }
+        shape.color = colorDetection();
+        shape.pointMin = pointMin;
+        shape.pointMax = pointMax;
+        shape.center = centroid;
+
+        shapes[shape.type].push_back(shape);
+    }
+
+    // DRAWING:
+    viewer->removeAllPointClouds(0);
+    viewer->removeAllShapes(0);
+    pcl::visualization::PointCloudColorHandlerRGBField<PointType> handler(
+            cloud_without_ground);
+    viewer->addCoordinateSystem(0.3, 0);
+    viewer->addPointCloud<PointType>(cloud_without_ground, handler, "sample cloud");
+    viewer->setPointCloudRenderingProperties(
+            pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3, "sample cloud");
+    for (int shapeType = 0; shapeType < 9; shapeType++) {
+    sort(shapes[shapeType].begin(), shapes[shapeType].end(), cmpByX);
+    for (int i = 0; i < shapes[shapeType].size(); i++) {
+        if (commandReceived && shouldDetect) {
+            if (command.shapeType == shapeType && command.index == i) {
+                cout << "GASITU-L-AM";
+                last_location = shapes[shapeType][i].center;
+                shouldDetect = false;
+            }
+        }
         j++;
-        cout << "Object " << j << ':' << cloud_object->points.size() << "points;\n";
-        string shape = detectShape();
         stringstream ss;
         ss << "box" << j;
+        rgb box_color;
+        if (shapes[shapeType][i].type == OUR_OBJECT) {
+            box_color.r = 1.0;
+            box_color.g = 0.0;
+            box_color.b = 0.0;
+        } else {
+            box_color.r = 1.0;
+            box_color.g = 1.0;
+            box_color.b = 1.0;
+        }
         viewer->addCube(
-                pointMin.x, pointMax.x,
-                pointMin.y, pointMax.y,
-                pointMin.z, pointMax.z,
-                1.0, 1.0, 1.0,
+                shapes[shapeType][i].pointMin.x, shapes[shapeType][i].pointMax.x,
+                shapes[shapeType][i].pointMin.y, shapes[shapeType][i].pointMax.y,
+                shapes[shapeType][i].pointMin.z, shapes[shapeType][i].pointMax.z,
+                box_color.r, box_color.g, box_color.b,
                 ss.str(), 0
         );
         ss << "text";
-        viewer->addText3D(shape.c_str(), pointMin, 0.1, 0.0, 1.0, 0.0, ss.str(), 0);
+        stringstream displayStr;
+        displayStr << shapeTypeToString(shapes[shapeType][i].type);
+        if (shapes[shapeType].size() > 1) {
+            displayStr << "_" << i + 1;
+        }
+        displayStr << "_" << getColor(shapes[shapeType][i].color);
+        viewer->addText3D(displayStr.str(), shapes[shapeType][i].pointMin, 0.1, 0.0, 1.0, 0.0, ss.str(), 0);
+        ss << "center";
+        viewer->addSphere(shapes[shapeType][i].center, 0.03, 1.0, 0.0, 0.0, ss.str(), 0);
     }
-    //while (!viewer->wasStopped()) {
-        viewer->spinOnce(100);
-    //    boost::this_thread::sleep(boost::posix_time::microseconds(100000));
-
-    //}
-    viewer->removeAllShapes(0);
+    }
+    viewer->spinOnce(100);
 }
 
 void cloud_cb(
         const boost::shared_ptr<const sensor_msgs::PointCloud2>& cloud_msg) {
+    clock_t start_time = clock();
     pcl::PCLPointCloud2 cloud_pcl;
     pcl_conversions::toPCL(*cloud_msg, cloud_pcl); //first from sensor msgs pointcloud2 to pcl pointcloud2
     pcl::fromPCLPointCloud2(cloud_pcl, *cloud); //from pcl pointcloud2 to pcl pointxyz in order to make to computations below
+
+    // Drawing:
+    pcl::visualization::PointCloudColorHandlerRGBField<PointType> handler_raw(
+            cloud);
+    viewer_raw->removeAllPointClouds(0);
+    viewer_raw->addPointCloud(cloud, handler_raw, "cloud_raw", 0);
+    viewer_raw->spinOnce(100);
 
     passthroughFilter();
     voxelGrid();
     findGround();
     euclidianClustering();
+    clock_t end_time = clock();
+    cout << "Detection took " << (double) (end_time - start_time) / CLOCKS_PER_SEC << "seconds\n";
+    ros::spinOnce();
+}
+
+void command_cb(const std_msgs::StringConstPtr& commandStr)
+{
+    string c = string(commandStr->data.c_str());
+    vector<string> tokens;
+    istringstream iss(c);
+    copy(istream_iterator<string>(iss),
+         istream_iterator<string>(),
+         back_inserter(tokens));
+    command.shapeType = UNKNOWN;
+    for (int i = 0; i < tokens.size(); i++) {
+        cout << tokens[i] << '\n';
+        if (tokens[i] == "box") {
+            command.shapeType = BOX;
+        } else if (tokens[i] == "cylinder") {
+            command.shapeType = CYLINDER;
+        } else if (tokens[i] == "person") {
+            // TODO: person
+            command.shapeType = PERSON;
+        } else if (tokens[i] == "sphere") {
+            command.shapeType = SPHERE;
+        }
+
+        if (tokens[i] == "first") {
+            command.index = 0;
+        } else if (tokens[i] == "second") {
+            command.index = 1;
+        } else if (tokens[i] == "third") {
+            command.index = 2;
+        } else if (tokens[i] == "fourth") {
+            command.index = 3;
+        }
+    }
+    if (command.shapeType == UNKNOWN) {
+        return;
+    }
+
+    commandReceived = true;
+    // TODO: process the string
+    cout << command.shapeType << '\n';
 }
 
 int main(int argc, char** argv) {
@@ -484,6 +686,9 @@ int main(int argc, char** argv) {
     // Create a ROS subscriber for the input point cloud
     ros::Subscriber sub = nh.subscribe("/camera/depth_registered/points", 1,
             cloud_cb);
+
+    // Service creation
+    ros::Subscriber sub2 = nh.subscribe("/robot/command", 1, command_cb);
 
     // Create a ROS publisher for the output point cloud
     pub = nh.advertise<sensor_msgs::PointCloud2>("output", 1);
